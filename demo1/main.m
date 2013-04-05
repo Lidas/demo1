11bae3e98b2839025d299795774feb964e57bc51
jimmy <lidas_sfs@hotmail.com>
2013-04-05 11:06:20 +0200

    Initial Commit

---
+++ demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo1
+//
+//  Created by Jimmy Lidström on 2013-04-05.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
