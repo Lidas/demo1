0d3a79d79e77231088b58d769316c0f61be297b2
jimmy <lidas_sfs@hotmail.com>
2013-04-05 11:08:36 +0200

    Added project

---
+++ demo1/DemoFile.h
@@ -0,0 +1,13 @@
+//
+//  DemoFile.h
+//  demo1
+//
+//  Created by Jimmy Lidström on 2013-04-05.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFile : NSObject
+
+@end
---
+++ demo1/DemoFile.m
@@ -0,0 +1,13 @@
+//
+//  DemoFile.m
+//  demo1
+//
+//  Created by Jimmy Lidström on 2013-04-05.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import "DemoFile.h"
+
+@implementation DemoFile
+
+@end
