92d93c5cf43a29aac5584e8c8800a0fd6df2b07e
jimmy <lidas_sfs@hotmail.com>
2013-04-05 11:18:48 +0200

    Added DemoFileX

---
+++ demo1/DemoFileX.h
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.h
+//  demo1
+//
+//  Created by Jimmy Lidström on 2013-04-05.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFileX : NSObject
+
+@end
---
+++ demo1/DemoFileX.m
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.m
+//  demo1
+//
+//  Created by Jimmy Lidström on 2013-04-05.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import "DemoFileX.h"
+
+@implementation DemoFileX
+
+@end
